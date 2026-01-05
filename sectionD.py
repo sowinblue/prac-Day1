30095f6768cc8fe813dd843ab2f89bef831ee4fd
송우인 <sowintildeblue@gmail.com>
2026-01-05 18:00:19 +0900

    D-5까지 완료

--- sectionD.py
+++ sectionD.py
@@ -21,3 +21,29 @@ music_genres_two.append(genre3)
 
 print(music_genres_two)
 
+
+# D-3 : 이름 입력받아 추가, 전체 개수 출력
+# 변수 이름 할당해서 하는 것이 유지 보수에 좋다
+gerne = input("장르를 입력: ")
+
+gerne_three=["jazz", "dance"]
+gerne_three.append(gerne)
+count = len(gerne_three)
+print(count)
+
+
+# D-4 : 반복문으로 여러 이름 입력받아 리스트 구성
+genres_four=[]
+counts= range(3)
+for i in counts:
+    genre = input("장르를 입력해!!: ")
+    genres_four.append(genre)
+
+print(genres_four)
+
+
+# D-5 : 리스트에서 특정 이름 검색
+genre = input("장르 조회: ")
+
+if genre in genres_four:
+    print(genre)
